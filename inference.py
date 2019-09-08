43765bfd374fd72c8dc0852c3c92758ea7851d58
mark <mark@b23.io>
2019-09-08 10:29:30 -0400

    Add home_dog feature back into the model
    This will allow for a linear relationship to capture negative spreads
    effectively

--- inference.py
+++ inference.py
@@ -19,10 +19,11 @@ def run_inference(model, scaler, time_sec, spread, favorite_points, underdog_poi
     payload = {}
     payload['time_elapsed_sec'] =  time_sec
     payload['spread'] = spread
+    payload['home_dog'] = int(spread < 0)
     payload['current_points_favorite'] = favorite_points
     payload['current_points_underdog'] = underdog_points
     df = pd.DataFrame.from_dict([payload])
-    
+
     scaled = scaler.transform(df)
     pred = model.predict(scaled)
     return pred[0][0]
