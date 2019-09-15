5eb8855aaa61cdc8cf2d8c320d1cd536ec50e507
mark <mark@b23.io>
2019-09-15 13:29:08 -0400

    Increase worker timeout for long tf initialization times

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 source env/bin/activate
-gunicorn --worker-class gevent --workers 1 --timeout 120 --bind 0.0.0.0:5000 server:app --daemon
+gunicorn --worker-class gevent --workers 1 --timeout 300 --bind 0.0.0.0:5000 server:app --daemon
