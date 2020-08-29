4d04a8d0bbf7f6f97791d2988c428dfb5981a9e6
Mark Bittmann <mbittmann@gmail.com>
2020-08-29 14:50:43 -0400

    Comment out inference stuff for performance reasons
    TF really slow to load during dev

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from werkzeug.exceptions import BadRequest, HTTPException
 import logging
 import logging.handlers as handlers
 import time
-import tensorflow as tf
-import keras
-import joblib
+# import tensorflow as tf
+# import keras
+# import joblib
 
 import nfl_api
 import inference 
@@ -26,13 +26,13 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 logHandler.setFormatter(formatter)
 app.logger.addHandler(logHandler)
 
-MODEL_PATH = 'model/model.h5'
-SCALER_PATH = 'model/scaler.pkl'
+# MODEL_PATH = 'model/model.h5'
+# SCALER_PATH = 'model/scaler.pkl'
 
-# load the model the model and scaler globally on startup
-global graph  # Allow the graph to be accessed within the app context
-graph = tf.get_default_graph()
-model =inference.load_model(MODEL_PATH)
+# # load the model the model and scaler globally on startup
+# global graph  # Allow the graph to be accessed within the app context
+# graph = tf.get_default_graph()
+# model =inference.load_model(MODEL_PATH)
 scaler = inference.load_scaler(SCALER_PATH)
 
 @app.route("/games/<year>/<phase>/<week>")
@@ -54,29 +54,29 @@ def get_game_data(year, phase, week):
     return return_val
 
 
-@app.route("/game/inference", methods=['POST'])
-def run_inference():
-    try:
-        input = request.get_json()
-    except BadRequest as e:
-        app.logger.error(e)
-        app.logger.error("Raw request body is: {}".format(request.get_data()))
-        return e.description
+# @app.route("/game/inference", methods=['POST'])
+# def run_inference():
+#     try:
+#         input = request.get_json()
+#     except BadRequest as e:
+#         app.logger.error(e)
+#         app.logger.error("Raw request body is: {}".format(request.get_data()))
+#         return e.description
 
-    app.logger.debug("JSON request body is: {}".format(input))
-    time_sec = int(input["time_sec"])
-    hspread = input["hspread"]  # The database stores spread relative to the home team
-    spread = -1*hspread  # The model uses spread relative to the visiting team
-    favorite_points = int(input["favorite_points"])
-    underdog_points = int(input["underdog_points"])
+#     app.logger.debug("JSON request body is: {}".format(input))
+#     time_sec = int(input["time_sec"])
+#     hspread = input["hspread"]  # The database stores spread relative to the home team
+#     spread = -1*hspread  # The model uses spread relative to the visiting team
+#     favorite_points = int(input["favorite_points"])
+#     underdog_points = int(input["underdog_points"])
 
-    start = time.time()
-    with graph.as_default():
-        pred = inference.run_inference(model, scaler, time_sec, spread, 
-                                       favorite_points, underdog_points)
-    end = time.time()
-    app.logger.debug("Finished in {} secs".format(end - start))
-    return {"cover_probability": float(pred)}
+#     start = time.time()
+#     with graph.as_default():
+#         pred = inference.run_inference(model, scaler, time_sec, spread, 
+#                                        favorite_points, underdog_points)
+#     end = time.time()
+#     app.logger.debug("Finished in {} secs".format(end - start))
+#     return {"cover_probability": float(pred)}
     
 
 if __name__ == "__main__":
